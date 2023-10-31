2712b83790d7e913ce92bb64482002ba92018792
MikusR <mikus.rakis@gmail.com>
2023-10-31 17:07:57 +0200

    MVP

--- index.php
+++ index.php
@@ -3,8 +3,20 @@
 declare(strict_types=1);
 
 use App\Models\Article;
+use App\Response;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require_once 'vendor/autoload.php';
+
+$loader = new FilesystemLoader('templates');
+$twig = new Environment($loader, [
+    'cache' => 'compilation_cache',
+    'debug' => true,
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+
 {
     $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $router) {
         $router->addRoute('GET', '/', ['App\Controllers\ArticleController', 'index']);
@@ -36,8 +48,15 @@ require_once 'vendor/autoload.php';
             $vars = $routeInfo[2];
             [$className, $method] = $handler;
             $id = (isset($vars['id'])) ? (int)$vars['id'] : null;
+            /**
+             * @var Response $response
+             */
             $response = (new $className())->{$method}($id);
-            var_dump($response);
+
+            $view = $response->getViewName();
+            $data = $response->getData();
+            echo $twig->render("$view.twig", $data);
+
             break;
     }
 }
